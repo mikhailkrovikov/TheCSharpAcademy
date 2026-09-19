a90a08445b5c82a2509056abb25d45d2c6869803
mkrovikov <mikhailkrovikov@gmail.com>
2026-09-19 17:46:24 +0300

    add microsoft calculator

---
+++ CalculatorLibrary/Calculator.cs
@@ -0,0 +1,85 @@
+﻿// CalculatorLibrary.cs
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    public class CalculationRecord
+    {
+        public readonly string Expression;
+        public readonly double Result;
+        public CalculationRecord(string expression, double result)
+        {
+            Expression = expression;
+            Result = result;
+        }
+    }
+
+    public class Calculator
+    {
+        private const int MaxHistorySize = 10;
+        public int UsageCount;
+        private readonly List<CalculationRecord> history = new();
+        private JsonWriter writer;
+        public IReadOnlyList<CalculationRecord> History => history.AsReadOnly();
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+
+        // CalculatorLibrary.cs
+        public double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    writer.WriteValue("Add");
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    writer.WriteValue("Subtract");
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    writer.WriteValue("Multiply");
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    writer.WriteValue("Divide");
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+    }
+}
