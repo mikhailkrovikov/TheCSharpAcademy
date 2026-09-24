0fa443a32c485054bd879db56d4fbc94208cb5f9
mkrovikov <mikhailkrovikov@gmail.com>
2026-09-24 21:34:57 +0300

    fix

---
+++ Coding.Tracker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Coding.Tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
